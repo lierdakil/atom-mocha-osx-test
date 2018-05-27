faef5cb3f8762646d6b80c89219bcb451fc456cf
Nikolay Yakimov <root@livid.pp.ru>
2018-05-27 16:50:46 +0300

    Simplify

---
+++ spec/activation.test.js
@@ -0,0 +1,16 @@
+import {expect} from 'chai'
+import path = require('path')
+
+describe('Activation', function () {
+  it('starts clean', async function () {
+    expect(window.atomMochaOSXTestPackageActivated).to.be.undefined
+  })
+  it('activates', async function () {
+    await atom.packages.activatePackage(path.join(__dirname, '..'))
+    expect(window.atomMochaOSXTestPackageActivated).to.be.true
+  })
+  it('deactivates', async function () {
+    await atom.packages.deactivatePackage('atom-mocha-osx-test')
+    expect(window.atomMochaOSXTestPackageActivated).to.be.false
+  })
+})
