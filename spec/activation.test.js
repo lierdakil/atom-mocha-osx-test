46162a816b2a950bff4f5c367d172a2e635e19ca
Nikolay Yakimov <root@livid.pp.ru>
2018-05-27 14:59:18 +0300

    Initial commit

---
+++ spec/activation.test.js
@@ -0,0 +1,18 @@
+'use babel'
+
+import {expect} from 'chai'
+const path = require('path')
+
+describe('Activation', function () {
+  it('starts clean', async function () {
+    expect(window.atomMochaOSXTestPackageActivated).to.be.undefined
+  })
+  it('activates', async function () {
+    await atom.packages.activatePackage(path.join(__dirname, '..'))
+    expect(window.atomMochaOSXTestPackageActivated).to.be.true
+  })
+  it('deactivates', async function () {
+    await atom.packages.deactivatePackage('atom-mocha-osx-test')
+    expect(window.atomMochaOSXTestPackageActivated).to.be.false
+  })
+})
